41fd4b0c5bdc7af3104a9e3495d6a11964a4ff82
snowserver <justin_snow@pennunited.com>
2016-01-28 13:07:50 -0500

    added method for Activity Class to get all activities for a user

--- lib/PipedrivePUT/activity.rb
+++ lib/PipedrivePUT/activity.rb
@@ -2,5 +2,13 @@ module PipedrivePUT
   class Activity
     include PipedrivePUT
 
+#----------------------------------------- Gets activites of a specific user----------------------------------------------------------
+    def self.getActivity(user_id)
+      @base = 'https://api.pipedrive.com/v1/activities/' + user_id.to_s + '?api_token=' + @@key.to_s
+      @content = open(@base.to_s).read
+      @parsed = JSON.parse(@content)
+      return @parsed
+    end
+#-------------------------------------------------------------------------------------------------------------------------------------
   end
 end

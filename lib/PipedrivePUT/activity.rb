4c86c784dc1a1917d6ea2a91d4c98ce931865c6b
kevTheDev <kev.j.edwards@gmail.com>
2016-05-23 14:47:14 +0100

    addActivity method signature

--- lib/PipedrivePUT/activity.rb
+++ lib/PipedrivePUT/activity.rb
@@ -21,8 +21,22 @@ module PipedrivePUT
       @content = open(@base.to_s).read
       @parsed = JSON.parse(@content)
       return @parsed
-    end 
+    end
 #-----------------------------------------------------------------------------------------------------------------------------------
+
+    #  subject (required) (string) Subject of the activity
+    #  done    (optional) (enumerated) Whether the activity is done or not. 0 = Not done, 1 = Done
+    #  type (required) (string) Type of the activity. This is in correlation with the key_string parameter of ActivityTypes.
+    #  due_date (optional) (date) Due date of the activity. Format: YYYY-MM-DD
+    #  due_time (optional) (time) Due time of the activity in UTC. Format: HH:MM
+    #  duration (optional) (time) Duration of the activity. Format: HH:MM
+    #  user_id (optional) (number) ID of the user whom the activity will be assigned to. If omitted, the activity will be assigned to the authorized user.
+    # deal_id (optional) (number) ID of the deal this activity will be associated with
+    # person_id (optional) (number) ID of the person this activity will be associated with
+    # org_id (optional) (number) ID of the organization this activity will be associated with
+    # note (optional) (string) Note of the activity (HTML format)
+    def self.addActivity(subject, type, options = {})
+    end
   end
 end
 

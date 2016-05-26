f8863500237f6c9637e22e6715f05c15b24d6e76
kevTheDev <kev.j.edwards@gmail.com>
2016-05-26 11:41:36 +0100

    do not include the term parameter if it is missing so we get back the correct API response instead of a bad request error

--- lib/PipedrivePUT/persons.rb
+++ lib/PipedrivePUT/persons.rb
@@ -85,17 +85,14 @@ module PipedrivePUT
       params = {}
 
       # optional search parameters
+      params[:term]            = term if term && !term.empty?
       params[:start]           = options.fetch(:start, 0)
       params[:org_id]          = options.fetch(:org_id, nil) if params[:org_id]
       params[:limit]           = options.fetch(:limit, 500)
       params[:search_by_email] = options.fetch(:search_by_email, 0)
       params[:api_token]       = @@key.to_s
 
-      url = "https://api.pipedrive.com/v1/persons/find?term=#{term}"
-
-      params.each do |key, value|
-        url << "&#{key}=#{value}"
-      end
+      url = "https://api.pipedrive.com/v1/persons/find?#{URI.encode_www_form(params)}"
 
       while more_items == true
         count = 0
